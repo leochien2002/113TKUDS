bff959b0575f61064473f81b9c115e653a8f7cdd
leochien2002 <a20045465@gmail.com>
2025-07-17 00:37:18 +0800

    add 0703

---
+++ 0703/oddnumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class oddnumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            sum += i;
+        }
+        
+        System.out.println(sum);
+        scanner.close();
+    }
+}
