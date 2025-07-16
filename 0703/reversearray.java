974ce54514b6b94ad765e9cb477faeabd6f016d6
leochien2002 <a20045465@gmail.com>
2025-07-17 00:39:38 +0800

    add 0703

---
+++ 0703/reversearray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class reversearray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+    
+        int start = 0, end = n - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i != n - 1) {
+                System.out.print(" ");
+            }
+        }
+        
+        scanner.close();
+    }
+}
